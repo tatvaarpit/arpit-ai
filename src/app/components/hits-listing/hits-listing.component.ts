b2ebc9865ec4c820aeafcdf6b96b83a1ec87fcac
Arpit Patel <arpit.patel@sparsh1.com>
2019-12-27 13:05:47 +0530

    Call api in 10 second interval

--- src/app/components/hits-listing/hits-listing.component.ts
+++ src/app/components/hits-listing/hits-listing.component.ts
@@ -15,87 +15,85 @@ import { map, startWith } from 'rxjs/operators';
 import { FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-hits-listing',
-  templateUrl: './hits-listing.component.html',
-  styleUrls: ['./hits-listing.component.css']
+    selector: 'app-hits-listing',
+    templateUrl: './hits-listing.component.html',
+    styleUrls: ['./hits-listing.component.css']
 })
 export class HitsListingComponent implements OnInit {
 
-  dataSource = new MatTableDataSource<Hit>();
-  displayedColumns = ['title', 'url', 'created_at', 'author'];
-  search : FormControl;
+    dataSource = new MatTableDataSource<Hit>();
+    displayedColumns = ['title', 'url', 'created_at', 'author'];
+    search: FormControl;
 
 
-  @ViewChild(MatSort) sort : MatSort;
-  @ViewChild(MatPaginator) paginator : MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private commonService: CommonService, private matDialog : MatDialog) {
-    // interval(10000)
-    //   .pipe(
-    //     startWith(0),
-    //     map(() => { this.commonService.getHitsListing() })
-    //   ).subscribe((hits) => {
-    //     if(typeof hits != typeof undefined){
-    //       this.dataSource = new MatTableDataSource(hits);
-    //       this.init();
-    //     }
-    //     }
-    //   );
-    this.commonService.getHitsListing().subscribe((hits) => {
-      if(typeof hits.hits != typeof undefined){
-        this.dataSource = new MatTableDataSource(hits.hits);
-        this.init();
-      }
-    });
-  }
-
-  ngOnInit() {
-    
-  }
-
-  /**
-   * Initialize table on data loads
-   *
-   * @memberof HitsListingComponent
-   */
-  init(){
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.filterPredicate = this.newFilterPredicate();
-  }
-
-  /**
-   * Filters table data by title
-   *
-   * @param {string} filter
-   * @memberof HitsListingComponent
-   */
-  applyFilter(filter : string){
-    this.dataSource.filter = JSON.stringify(filter.trim().toLowerCase());
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+    constructor(private commonService: CommonService, private matDialog: MatDialog) {
+        interval(1000).pipe( startWith(0),() => this.commonService.getHitsListing()).subscribe(
+            (hits) => {
+                console.log(hits);
+                if (typeof hits.hits != typeof undefined) {
+                    this.dataSource = new MatTableDataSource(hits.hits);
+                    this.init();
+                }
+            }
+        );
     }
-  }
 
+    ngOnInit() {
 
-  /**
-   * Override filter predicate
-   *
-   * @returns
-   * @memberof HitsListingComponent
-   */
-  newFilterPredicate(){
-    let filterFunction = (data : any, filter : string) : boolean =>{
-      let searchData = JSON.parse(filter);
-      return data.title.trim().toLowerCase().indexOf(searchData.trim().toLowerCase()) !== -1;
     }
-    return filterFunction;
-  }
 
-  openDialog(row : Hit){
-    this.matDialog.open(DialogComponent,{
-      data : { rowJson : row }
-    });
-  }
+    /**
+     * Initialize table on data loads
+     *
+     * @memberof HitsListingComponent
+     */
+    init() {
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.filterPredicate = this.newFilterPredicate();
+    }
+
+    /**
+     * Filters table data by title
+     *
+     * @param {string} filter
+     * @memberof HitsListingComponent
+     */
+    applyFilter(filter: string) {
+        this.dataSource.filter = JSON.stringify(filter.trim().toLowerCase());
+        if (this.dataSource.paginator) {
+            this.dataSource.paginator.firstPage();
+        }
+    }
+
+
+    /**
+     * Override filter predicate
+     *
+     * @returns
+     * @memberof HitsListingComponent
+     */
+    newFilterPredicate() {
+        let filterFunction = (data: any, filter: string): boolean => {
+            let searchData = JSON.parse(filter);
+            return data.title.trim().toLowerCase().indexOf(searchData.trim().toLowerCase()) !== -1;
+        }
+        return filterFunction;
+    }
+
+    /**
+     * Open dialog on table row click
+     *
+     * @param {Hit} row
+     * @memberof HitsListingComponent
+     */
+    openDialog(row: Hit) {
+        this.matDialog.open(DialogComponent, {
+            data: { rowJson: row }
+        });
+    }
 
 }
